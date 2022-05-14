4a3c5c53894b4619a4ef8b4a84f0c36a7da20ab2
ik-top-digital <top.digital.ik@gmail.com>
2022-05-14 09:29:06 +0300

    lottie animation on native splash screen

--- android/app/src/main/kotlin/com/example/native_splash_lottie/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_splash_lottie/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.native_splash_lottie
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
